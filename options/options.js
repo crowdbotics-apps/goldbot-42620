e4913b3e6d6f2eb3c5a910eda552b9564257f611
Crowdbotics <team@crowdbotics.com>
2023-08-03 18:28:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "goldbot-42620",
+  url: "https://goldbot-42620.botics.co",
+  api: "https://goldbot-42620.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
